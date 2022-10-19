974f1e1fe686c3038bf9b48eb8f7547da1f3e70f
rich <adukyerer@gmail.com>
2022-10-19 21:11:03 +0000

    Add has many to category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
     belongs_to :user
     has_many :budget_transactions, through: :category_transactions
-    
+    has_many :category_transactions, dependent: :destroy
+
 end
