085363adaf4bdb75e115f9d496ad651445ef4a2b
rich <adukyerer@gmail.com>
2022-10-19 20:55:25 +0000

    Add users associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :budget_transactions,foreign_key: "user_id", class_name: "BudgetTransaction", dependent: :destroy
+    has_many :categories, foreign_key: "user_id", class_name: "Category", dependent: :destroy
 end
