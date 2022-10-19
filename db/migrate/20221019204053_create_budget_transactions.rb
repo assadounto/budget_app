e86eca9932489ba8e281c0c358e3da96eb9d3997
rich <adukyerer@gmail.com>
2022-10-19 21:44:35 +0000

    Add migration and schema

--- db/migrate/20221019204053_create_budget_transactions.rb
+++ db/migrate/20221019204053_create_budget_transactions.rb
@@ -1,7 +1,9 @@
 class CreateBudgetTransactions < ActiveRecord::Migration[7.0]
   def change
     create_table :budget_transactions do |t|
-
+      t.references :user, null: false, foreign_key: true
+      t.string :name
+      t.decimal :amount
       t.timestamps
     end
   end
--- db/migrate/20221019204313_create_category_transactions.rb
+++ db/migrate/20221019204313_create_category_transactions.rb
@@ -1,7 +1,8 @@
 class CreateCategoryTransactions < ActiveRecord::Migration[7.0]
   def change
     create_table :category_transactions do |t|
-
+      t.references :category, null: false, foreign_key: true
+      t.references :budget_transaction, null: false, foreign_key: true
       t.timestamps
     end
   end
